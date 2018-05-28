54fff935ebb3af0a4a57246cc5161cac493324c4
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2018-05-28 08:56:31 -0400

    MAINT: Ensure signatures with frozen sizes written differently compare equal.
    Was comparison by the string representation so that "03" and "3" would
    be considered different dimensions.  That would be surprising for implementing
    an inner loop...

--- numpy/core/tests/test_ufunc.py
+++ numpy/core/tests/test_ufunc.py
@@ -342,6 +342,13 @@ class TestUfunc(object):
         assert_equal(flags, (1, 0, 1))
         assert_equal(sizes, (3, -1, 9))
 
+        enabled, num_dims, ixs, flags, sizes = umt.test_signature(3, 1, "(3),(03,3),(n)->(9)")
+        assert_equal(enabled, 1)
+        assert_equal(num_dims, (1, 2, 1, 1))
+        assert_equal(ixs, (0, 0, 0, 1, 2))
+        assert_equal(flags, (1, 0, 1))
+        assert_equal(sizes, (3, -1, 9))
+
         enabled, num_dims, ixs, flags, sizes = umt.test_signature(3, 1, "(3?),(3?,3?),(n)->(9)")
         assert_equal(enabled, 1)
         assert_equal(num_dims, (1, 2, 1, 1))
